600622a81ae7c8be0de035afa88059073eddc70d
efontdecaba <efontdecaba@gmail.com>
2014-10-22 23:01:53 +0200

    Update cachematrix.R
    First version of EF. Checked in R Studio, as you can see here:
    
    > myMat$set(matrix(c(1,24,13,41,53,62,71,88,93),nrow=3,ncol=3))
    > cacheSolve(myMat)
                [,1]        [,2]        [,3]
    [1,] -0.04545455  0.05080214 -0.01336898
    [2,] -0.09384164 -0.07158875  0.13938244
    [3,]  0.06891496  0.04062446 -0.08030016
    > cacheSolve(myMat)
    getting cached data
                [,1]        [,2]        [,3]
    [1,] -0.04545455  0.05080214 -0.01336898
    [2,] -0.09384164 -0.07158875  0.13938244
    [3,]  0.06891496  0.04062446 -0.08030016

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## This function will contain de 4 functions to set, get the values of the matrix 
+## and to get and set the invere.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+
+    Y <- NULL ## I define the matrix that will contain the inverse
+    set <- function(y) {
+      x <<- y
+      Y <<- NULL
+    }# This function gets the matrix and caches it.
+    
+    get <- function() x ## This function returns the matrix
+    setinverse <- function(inv) Y <<- inv ## This function stores the inverse
+    getinverse <- function() Y ## This funcion returns the inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
 ## Write a short comment describing this function
 
+## This function makes the inverse and stores it. if called again, it returns the inverse
+## without new computation
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    inv <- x$getinverse() ## It checks werther the inverse is already computed
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mat <- x$get()  ## get the matrix to invert
+    inv <- solve(mat) ## computes the inverse
+    x$setinverse(inv)  ## stores the inverse (caches it) for further access
+    inv  ## Returns the inverse
+
 }
